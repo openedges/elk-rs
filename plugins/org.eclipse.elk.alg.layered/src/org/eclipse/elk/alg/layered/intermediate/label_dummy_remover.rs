45d8670f32fe3ad14b8a0c156e0e5753659d18f8
luuvish <luuvish@gmail.com>
2026-02-16 21:08:38 +0900

    layered: adjust center label dummy x for post-compaction

--- plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/intermediate/label_dummy_remover.rs
+++ plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/intermediate/label_dummy_remover.rs
@@ -4,11 +4,14 @@ use org_eclipse_elk_core::org::eclipse::elk::core::options::core_options::CoreOp
 use org_eclipse_elk_core::org::eclipse::elk::core::options::direction::Direction;
 use org_eclipse_elk_core::org::eclipse::elk::core::options::edge_routing::EdgeRouting;
 use org_eclipse_elk_core::org::eclipse::elk::core::options::label_side::LabelSide;
+use org_eclipse_elk_core::org::eclipse::elk::core::options::port_side::PortSide;
 use org_eclipse_elk_core::org::eclipse::elk::core::util::IElkProgressMonitor;
 
 use crate::org::eclipse::elk::alg::layered::graph::{LGraph, LLabelRef, LNode, LNodeRef, NodeType};
 use crate::org::eclipse::elk::alg::layered::intermediate::LongEdgeJoiner;
-use crate::org::eclipse::elk::alg::layered::options::{InternalProperties, LayeredOptions, Origin};
+use crate::org::eclipse::elk::alg::layered::options::{
+    GraphCompactionStrategy, InternalProperties, LayeredOptions, Origin, Spacings,
+};
 
 pub struct LabelDummyRemover;
 
@@ -28,6 +31,10 @@ impl ILayoutProcessor<LGraph> for LabelDummyRemover {
         let edge_routing = layered_graph
             .get_property(LayeredOptions::EDGE_ROUTING)
             .unwrap_or(EdgeRouting::Orthogonal);
+        let compaction_strategy = layered_graph
+            .get_property(LayeredOptions::COMPACTION_POST_COMPACTION_STRATEGY)
+            .unwrap_or(GraphCompactionStrategy::None);
+        let spacings = layered_graph.get_property(InternalProperties::SPACINGS);
 
         let layers = layered_graph.layers().clone();
         for layer in layers {
@@ -52,6 +59,8 @@ impl ILayoutProcessor<LGraph> for LabelDummyRemover {
                     edge_label_spacing,
                     label_label_spacing,
                     layout_direction,
+                    compaction_strategy,
+                    spacings.as_ref(),
                 );
 
                 LongEdgeJoiner::join_at(&node, edge_routing == EdgeRouting::Polyline);
@@ -69,6 +78,8 @@ impl LabelDummyRemover {
         edge_label_spacing: f64,
         label_label_spacing: f64,
         layout_direction: Direction,
+        compaction_strategy: GraphCompactionStrategy,
+        spacings: Option<&Spacings>,
     ) {
         let (origin_edge, represented_labels, mut curr_label_pos, node_size, labels_below_edge, inline_labels) = {
             let mut node_guard = match node.lock() {
@@ -115,6 +126,16 @@ impl LabelDummyRemover {
             )
         };
 
+        if !layout_direction.is_vertical() {
+            Self::adjust_horizontal_dummy_position_for_post_compaction(
+                node,
+                &mut curr_label_pos,
+                node_size.x,
+                compaction_strategy,
+                spacings,
+            );
+        }
+
         let label_space = KVector::with_values(
             node_size.x,
             node_size.y
@@ -147,6 +168,91 @@ impl LabelDummyRemover {
         if let Ok(mut edge_guard) = origin_edge.lock() {
             edge_guard.labels_mut().extend(represented_labels);
         };
+    }
+
+    fn adjust_horizontal_dummy_position_for_post_compaction(
+        node: &LNodeRef,
+        curr_label_pos: &mut KVector,
+        node_width: f64,
+        compaction_strategy: GraphCompactionStrategy,
+        spacings: Option<&Spacings>,
+    ) {
+        if compaction_strategy != GraphCompactionStrategy::Left
+            && compaction_strategy != GraphCompactionStrategy::Right
+        {
+            return;
+        }
+        let Some(spacings) = spacings else {
+            return;
+        };
+        let Some((source_node, target_node)) = Self::adjacent_source_and_target_nodes(node) else {
+            return;
+        };
+
+        let source_right = source_node
+            .lock()
+            .ok()
+            .map(|mut source_guard| {
+                source_guard.shape().position_ref().x + source_guard.shape().size_ref().x
+            });
+        let target_left = target_node
+            .lock()
+            .ok()
+            .map(|mut target_guard| target_guard.shape().position_ref().x);
+        let (Some(source_right), Some(target_left)) = (source_right, target_left) else {
+            return;
+        };
+
+        let left_spacing = spacings.get_horizontal_spacing(&source_node, node);
+        let right_spacing = spacings.get_horizontal_spacing(node, &target_node);
+        let left_bound = source_right + left_spacing;
+        let right_bound = target_left - right_spacing - node_width;
+        if !left_bound.is_finite() || !right_bound.is_finite() {
+            return;
+        }
+
+        if left_bound > right_bound {
+            curr_label_pos.x = left_bound;
+            return;
+        }
+        curr_label_pos.x = if compaction_strategy == GraphCompactionStrategy::Left {
+            left_bound
+        } else {
+            right_bound
+        };
+    }
+
+    fn adjacent_source_and_target_nodes(node: &LNodeRef) -> Option<(LNodeRef, LNodeRef)> {
+        let (west_port, east_port) = {
+            let node_guard = node.lock().ok()?;
+            (
+                node_guard.ports_by_side(PortSide::West).first().cloned(),
+                node_guard.ports_by_side(PortSide::East).first().cloned(),
+            )
+        };
+        let incoming_edge = west_port.and_then(|port| {
+            port.lock()
+                .ok()
+                .and_then(|port_guard| port_guard.incoming_edges().first().cloned())
+        })?;
+        let outgoing_edge = east_port.and_then(|port| {
+            port.lock()
+                .ok()
+                .and_then(|port_guard| port_guard.outgoing_edges().first().cloned())
+        })?;
+
+        let source_node = incoming_edge
+            .lock()
+            .ok()
+            .and_then(|edge_guard| edge_guard.source())
+            .and_then(|port| port.lock().ok().and_then(|port_guard| port_guard.node()))?;
+        let target_node = outgoing_edge
+            .lock()
+            .ok()
+            .and_then(|edge_guard| edge_guard.target())
+            .and_then(|port| port.lock().ok().and_then(|port_guard| port_guard.node()))?;
+
+        Some((source_node, target_node))
     }
 
     fn place_labels_for_horizontal_layout(
